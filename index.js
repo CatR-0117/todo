7f108d3d8c3f6a5ff4efaaf65001a9f6a71e540e
CatR-0117 <nandianandia149@gmail.com>
2026-04-21 08:03:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 // taskcontainer text uurclugddug
 // task ustgadag -- 3 turluur 
 // section soligddog style uurckugdunu
+
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+function addTask() {
+  const value = taskInput.value.trim();
+  if (value === "") return;
+  const li = document.createElement("li");
+  li.textContent = value;
+  taskList.appendChild(li);
+  taskInput.value = "";
+}
